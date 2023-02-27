f45dec562fe4ca07e73571101602fc3c3fff970c
evievermeer <evermeer@bren.ucsb.edu>
2023-02-27 10:12:32 -0800

    making crop yield function

---
+++ R/crop_yield.R
@@ -0,0 +1,9 @@
+#' Crop output function 
+
+#' This function determines the crop output/yield of a given sample
+#' @param fertilizer fertilizer applied (kg)
+#' @param TP is total precipitation (cm)
+#' @return crop yield
+
+
+
