b93b2a366f6fae78698e068518e17737377fdcc8
evievermeer <evermeer@bren.ucsb.edu>
2023-02-28 13:32:08 -0800

    Completed crop yield function

--- R/crop_yield.R
+++ R/crop_yield.R
@@ -2,8 +2,12 @@
 
 #' This function determines the crop output/yield of a given sample
 #' @param fertilizer fertilizer applied (kg)
-#' @param TP is total precipitation (cm)
+#' @param tp is total precipitation (cm)
 #' @return crop yield
 
-
+# function definition
+crop_yield = function(fertilizer, tp)  {
+  result = 1.8 * (fertilizer^2)- 0.5 * fertilizer + 0.1 * tp
+  return(result)
+}
 
