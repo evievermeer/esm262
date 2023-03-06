2a97edb334e9f3518159afe10ff62fd6e9668d23
evievermeer <evermeer@bren.ucsb.edu>
2023-03-06 11:51:02 -0800

    fish function is broken for now. Will circle back

--- R/fish_classification.R
+++ R/fish_classification.R
@@ -4,24 +4,23 @@
 #'  - the most common fish 
 #'  - the rarest fish 
 #'  - the total number of fish 
-#'  
-#'  
-#'  
-#'  
-#' 
-#' 
-#' 
-#' @param V vehicle speed (m/s)
-#' @param m vehicle mass 
-#' @param A surface area of car (m2)
-#' @param g acceleration due to gravity (m/s) default=9.8
-#' @param p_air  is density of air (kg/m3) default= 1.2
-#' @param crolling  is the rolling resistance coefficient default= .015
-#' @param cdrag  is the aerodynamic resistance coefficient default= .3
-#' @return efficiency (W)
-#'
-# function definition
-auto_fuel_eff = function(crolling=.015, m, g=9.8, V, A, p_air=1.2, cdrag=0.3)  {
-  result = crolling * m * g * V + (1/2) * A * p_air * cdrag * (V^3)
-  return(result)
+#'  d
+
+fish_classification = function(fish) {
+fish = as.factor(fish)
+
+#' which is the most common
+tmp = which.max(summary(fish))
+most_common = names(summary(fish)[tmp])
+
+#'  # which is the most rare
+tmp = which.min(summary(fish))
+rarest = names(summary(fish)[tmp])
+
+#'  # number of fish 
+nfish = length(summary(fish)
+#' return result 
+return(list(most_common=most_common,rarest = rarest,nfish = nfish))
 }
+
+
