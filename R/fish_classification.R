6c33ae963cb09a24e2bcdfd3ddfd485bc2e5a5e0
evievermeer <evermeer@bren.ucsb.edu>
2023-03-07 15:18:54 -0800

    finished fish assignment

--- R/fish_classification.R
+++ R/fish_classification.R
@@ -4,18 +4,22 @@
 #'  - the most common fish 
 #'  - the rarest fish 
 #'  - the total number of fish 
-#'  d
-
+#'  
+#'  #first, process the data to a vector, then factor form
 fish_classification = function(fish) {
 fish_vect = as.vector((fish[,1]))  
 fish_fact = as.factor(fish_vect)
 
+#' create code to return which fish name is most common 
 most_common_fish <- names(which.max(summary(fish_fact)))
 
+#' create code to return which fish name is rarest
 rarest_fish <- names(which.min(summary(fish_fact)))
 
+#' create code to return the number of fish
 total_number <- sum(summary(fish_fact))
 
+#' create code to aggregate the results, then use return() to show results
 final_list <- c(most_common_fish,rarest_fish,total_number)
 
 return(final_list)
